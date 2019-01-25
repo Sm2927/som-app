242933658543700af37edcd3a0e481077a620d8d
Sm2927 <44033974+Sm2927@users.noreply.github.com>
2019-01-25 17:35:46 +0530

    with user authentication and flash messages

--- app1.py
+++ app1.py
@@ -1,10 +1,12 @@
-from flask import Flask,render_template,redirect,url_for,request
+from flask import Flask,render_template,redirect,url_for,request, session, flash
 
 app =  Flask(__name__)
 
+app.secret_key = "sm sm"
+
 @app.route('/')
 def home():
-    return "Hey Sovna!"
+    return render_template('home.html')
 
 @app.route('/comein')
 def comein():
@@ -17,8 +19,16 @@ def login():
         if request.form['username'] != 'admin' or request.form['password'] != 'password':
             err = 'Invalid username or password. Please try again.'
         else:
-            return redirect(url_for('home'))
+            session['logged_in'] = True
+            flash("hey! you have successfully logged in!")
+            return redirect(url_for('comein'))
     return render_template('login.html',error=err)
+
+@app.route('/logout')
+def logout():
+    session.pop('logged_in',None)
+    flash("hey! you have successfully logged out!")
+    return redirect(url_for('home'))
 
 if __name__ ==  '__main__':
     app.run(debug=True)
